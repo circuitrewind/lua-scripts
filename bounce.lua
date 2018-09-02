2a6d2c826b6e930e81d79e56bf38215c808151e3
Vincent Milum Jr <git@darkain.com>
2018-09-01 22:39:40 -0700

    Fixing the "bounce" script

--- bounce.lua
+++ bounce.lua
@@ -1,9 +1,9 @@
-x		= 4
-y		= 5
+x		= esp.random(1, led.width(0) - 2)
+y		= esp.random(1, led.height(0) - 2)
 xdir	= esp.random(5,30) / 100
 ydir	= esp.random(5,30) / 100
-color	= led.color(esp.random(0,0x0f))
-fade	= 0;
+rgb		= color.palette(2)
+fade	= 0
 
 function frame()
 	x = x + xdir
@@ -12,21 +12,21 @@ function frame()
 	if x >= led.width(0) then
 		x		= led.width(0) - 1
 		xdir	= esp.random(-30,-5) / 100
-		color	= led.color(esp.random(0, 0x0f))
+		rgb		= color.palette(esp.random(0, 0x0f))
 	elseif x < 0 then
 		x		= 0
 		xdir	= esp.random(5, 30) / 100
-		color	= led.color(esp.random(0, 0x0f))
+		rgb		= color.palette(esp.random(0, 0x0f))
 	end
 
 	if y >= led.height(0) then
 		y		= led.height(0) - 1
 		ydir	= esp.random(-30,-5) / 100
-		color	= led.color(esp.random(0, 0x0f))
+		rgb		= color.palette(esp.random(0, 0x0f))
 	elseif y < 0 then
 		y		= 0
 		ydir	= esp.random(5, 30) / 100
-		color	= led.color(esp.random(0, 0x0f))
+		rgb		= color.palette(esp.random(0, 0x0f))
 	end
 
 	fade = fade + 1
@@ -35,5 +35,5 @@ function frame()
 		fade = 0
 	end
 
-	led.draw(0, x, y, color)
+	led.draw(0, x, y, rgb)
 end
